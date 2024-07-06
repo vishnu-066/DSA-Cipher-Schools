d6e12c8db4260233201a0bdeb9478cab2728d393
Vishnu Vardhan Reddy <118803764+vishnu-066@users.noreply.github.com>
2024-07-06 12:14:35 +0530

    Add files via upload

---
+++ lecture_19.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"size of int : "<<sizeof(int)<<endl;
+	cout<<"size of short int : "<<sizeof(long int)<<endl;
+	cout<<"size of long long int : "<<sizeof(long long int)<<endl;
+	cout<<"size of float : "<<sizeof(float)<<endl;
+	cout<<"size of char : "<<sizeof(char)<<endl;
+	cout<<"size of double : "<<sizeof(double)<<endl;
+	cout<<"size of bool : "<<sizeof(bool)<<endl;
+	return 0;
+}
